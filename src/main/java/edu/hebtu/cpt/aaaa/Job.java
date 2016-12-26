9c49c597433961985d3addbed64b810ac9ed6759
Leejikun <835304330@qq.com>
2016-12-26 16:09:29 +0800

    終極版

---
+++ src/main/java/edu/hebtu/cpt/aaaa/Job.java
@@ -0,0 +1,121 @@
+package edu.hebtu.cpt.aaaa;
+
+/**
+ * Created by lenovo on 2016/12/20.
+ */
+public class Job {
+private String request;
+
+private String address;
+
+private String num;
+
+private String stoptime;
+
+private int type;
+
+private String rijieyuejie;
+
+private String money;
+
+private String name;
+
+private String company;
+
+private int id;
+
+private String time;
+
+private String thing;
+
+private String user;
+
+private String status;
+
+public void setRequest(String request){
+this.request = request;
+}
+public String getRequest(){
+return this.request;
+}
+public void setAddress(String address){
+this.address = address;
+}
+public String getAddress(){
+return this.address;
+}
+public void setNum(String num){
+this.num = num;
+}
+public String getNum(){
+return this.num;
+}
+public void setStoptime(String stoptime){
+this.stoptime = stoptime;
+}
+public String getStoptime(){
+return this.stoptime;
+}
+public void setType(int type){
+this.type = type;
+}
+public int getType(){
+return this.type;
+}
+public void setRijieyuejie(String rijieyuejie){
+this.rijieyuejie = rijieyuejie;
+}
+public String getRijieyuejie(){
+return this.rijieyuejie;
+}
+public void setMoney(String money){
+this.money = money;
+}
+public String getMoney(){
+return this.money;
+}
+public void setName(String name){
+this.name = name;
+}
+public String getName(){
+return this.name;
+}
+public void setCompany(String company){
+this.company = company;
+}
+public String getCompany(){
+return this.company;
+}
+public void setId(int id){
+this.id = id;
+}
+public int getId(){
+return this.id;
+}
+public void setTime(String time){
+this.time = time;
+}
+public String getTime(){
+return this.time;
+}
+public void setThing(String thing){
+this.thing = thing;
+}
+public String getThing(){
+return this.thing;
+}
+public void setUser(String user){
+this.user = user;
+}
+public String getUser(){
+return this.user;
+}
+public void setStatus(String status){
+this.status = status;
+}
+public String getStatus(){
+return this.status;
+}
+}
+
+
